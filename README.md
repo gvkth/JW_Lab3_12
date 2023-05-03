409c6b3e9d54d02c3a40d6b6a3ba1862859b49cc
cuongvk <cuongvk.hcm@vnpt.vn>
2023-05-03 20:52:11 +0700

    Lab 3.12 - start

---
+++ README.md
@@ -0,0 +1,2 @@
+# JW_Lab3_12
+ Thực hành với Constructor Injection
---
+++ src/com/luv2code/springdemo/Main.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("tennisCoach",Coach.class);
+        //call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //call method to get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
+
+        //close the context
+        context.close();
+    }
+}
